12bd3127c020bfbde23bdece4a0e67ef43c6f50c
YasmeenTak <yasmeen.tak.33@gmail.com>
2020-11-22 10:29:46 +0200

    fixing

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -10,12 +10,6 @@ $(document).ready(function () {
   $('.fullscreen').css('height', window_height);
   $('.fitscreen').css('height', fitscreen);
 
-  // ------- Datepicker  js --------//
-
-  $(function () {
-    $('.date-picker').datepicker();
-  });
-
   //------- Niceselect  js --------//
 
   if (document.getElementById('default-select')) {
@@ -27,89 +21,6 @@ $(document).ready(function () {
   if (document.getElementById('service-select')) {
     $('select').niceSelect();
   }
-
-  //------- Lightbox  js --------//
-
-  $('.img-gal').magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true,
-    },
-  });
-
-  $('.play-btn').magnificPopup({
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false,
-  });
-
-  //------- Superfish nav menu  js --------//
-
-  $('.nav-menu').superfish({
-    animation: {
-      opacity: 'show',
-    },
-    speed: 400,
-  });
-
-  //------- Owl Carusel  js --------//
-
-  $('.active-hot-deal-carusel').owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: true,
-    autoplayHoverPause: true,
-    smartSpeed: 500,
-    margin: 30,
-    dots: true,
-  });
-
-  $('.active-testimonial').owlCarousel({
-    items: 2,
-    loop: true,
-    margin: 30,
-    autoplayHoverPause: true,
-    smartSpeed: 500,
-    dots: true,
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      480: {
-        items: 1,
-      },
-      992: {
-        items: 2,
-      },
-    },
-  });
-
-  $('.active-recent-blog-carusel').owlCarousel({
-    items: 3,
-    loop: true,
-    margin: 30,
-    dots: true,
-    autoplayHoverPause: true,
-    smartSpeed: 500,
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      480: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      961: {
-        items: 3,
-      },
-    },
-  });
 
   //------- Mobile Nav  js --------//
 
@@ -450,7 +361,7 @@ $(document).ready(function () {
   //------- Mailchimp js --------//
 
   $(document).ready(function () {
-    $('#mc_embed_signup').find('form').ajaxChimp();
+    // $('#mc_embed_signup').find('form').ajaxChimp();
   });
 });
 
