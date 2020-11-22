2dada15b9f08df846a3a60f71bb452f1d0481513
YasmeenTak <yasmeen.tak.33@gmail.com>
2020-11-22 11:00:24 +0200

    fixing

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -360,9 +360,9 @@ export class Home extends Component {
                       </ul>
                     </div>
                   </div>
-                  <div class='col-lg-6 insurence-right'>
+                  <div className='col-lg-6 insurence-right'>
                     <img
-                      class='img-fluid img-two'
+                      className='img-fluid img-two'
                       src='assets/img/details/sell2.web'
                       alt='sell cars'
                     />
@@ -456,9 +456,8 @@ export class Home extends Component {
 
                 <div className='row footer-bottom d-flex justify-content-between align-items-center'>
                   <p className='col-lg-8 col-sm-12 footer-text m-0'>
-                    Copyright &copy;
-                    <script>document.write(new Date().getFullYear());</script>
-                    All rights reserved | This template is made with
+                    Copyright &copy; All rights reserved | This template is made
+                    with
                     <i className='fa fa-heart-o' aria-hidden='true'></i> by
                     <a href='http://cashcars.surge.sh/' target='_blank'>
                       Yasmeen
